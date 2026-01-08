8794a36728fa451efccaed1a2dee120553d5518c
Feng Tu <alonza.tu@gmail.com>
2026-01-08 15:03:15 +0800

    chore: update test name in mp

--- test/test-ulcl-mp.sh
+++ test/test-ulcl-mp.sh
@@ -5,7 +5,7 @@
 # usage:
 # ./test-ulcl-mp.sh <test-name>
 #
-# e.g. ./test-ulcl-mp.sh <TestULCLMultiPathCi1|TestULCLMultiPathCi2>
+# e.g. ./test-ulcl-mp.sh <TestULCLMultiPathUe1|TestULCLMultiPathUe2>
 #
 ##########################
 
@@ -14,17 +14,17 @@ echo "test $1"
 target_webconsole_subscription_data_file=""
 
 case "$1" in
-    "TestULCLMultiPathCi1")
+    "TestULCLMultiPathUe1")
         target_webconsole_subscription_data_file="json/webconsole-subscription-data-mp-1.json"
     ;;
-    "TestULCLMultiPathCi2")
+    "TestULCLMultiPathUe2")
         target_webconsole_subscription_data_file="json/webconsole-subscription-data-mp-2.json"
     ;;
 esac
 
 echo "target_webconsole_subscription_data_file: $target_webconsole_subscription_data_file"
 
-# post ue (ci-test PacketRusher) data to db
+# post ue data to db
 ./api-webconsole-subscribtion-data-action.sh post $target_webconsole_subscription_data_file
 if [ $? -ne 0 ]; then
     echo "Failed to post subscription data"
@@ -37,7 +37,7 @@ go test -v -vet=off -run $1
 go_test_exit_code=$?
 cd ..
 
-# delete ue (ci-test PacketRusher) data from db
+# delete ue data from db
 ./api-webconsole-subscribtion-data-action.sh delete $target_webconsole_subscription_data_file
 if [ $? -ne 0 ]; then
     echo "Failed to delete subscription data"
